ea5d9fc606c948d73db5f9c367944754318b4c73
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2022-12-30 12:34:53 +0100

    chore: remove native

--- src/app.rs
+++ src/app.rs
@@ -1,25 +1,14 @@
-use std::sync::mpsc::{Receiver, Sender};
 use translation_server_client_silen::ready;
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 pub struct TemplateApp {
-    tx: Sender<String>,
-
-    rx: Receiver<String>,
-
     open: bool,
-    translation_server_ready: String,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
-        let (tx, rx) = std::sync::mpsc::channel();
-
         Self {
-            tx,
-            rx,
             open: false,
-            translation_server_ready: "nothing".to_owned(),
         }
     }
 }
@@ -34,10 +23,7 @@ impl TemplateApp {
 impl eframe::App for TemplateApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let Self {
-            tx: _,
-            rx: _,
             open: _,
-            translation_server_ready,
         } = self;
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
@@ -57,39 +43,16 @@ impl eframe::App for TemplateApp {
             });
         });
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label(&*translation_server_ready);
-        });
 
         if self.open {
             egui::Window::new("Window").show(ctx, |ui| {
                 if ui.button("Close").clicked() {
                     self.open = false;
                 }
-
-                if ui.button("is server ready?").clicked() {
-                    send_req(self.tx.clone(), ctx.clone());
-                }
             });
         }
 
-        if let Ok(ready_status) = self.rx.try_recv() {
-            self.translation_server_ready = ready_status;
-        }
+    
     }
 }
 
-fn send_req(tx: Sender<String>, ctx: egui::Context) {
-    tokio::spawn(async move {
-        // Send a request with an increment value.
-        let response = ready().await;
-
-        let result = match response {
-            Ok(v) => v.text().await.map_err(|e| e.to_string()).unwrap(),
-            Err(e) => e,
-        };
-        let _ = tx.send(result);
-
-        ctx.request_repaint();
-    });
-}
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,7 @@
 #![warn(clippy::all, rust_2018_idioms)]
-use std::time::Duration;
 
-#[cfg(target_arch = "wasm32")]
-use omnis_vanitas::TemplateApp;
-use tokio::runtime::Runtime;
 
-// When compiling natively:
-//[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    let rt = Runtime::new().expect("Unable to create Runtime");
-
-    // Enter the runtime so that `tokio::spawn` is available immediately.
-    let _enter = rt.enter();
-
-    // Execute the runtime in its own thread.
-    // The future doesn't have to do anything. In this example, it just sleeps forever.
-    std::thread::spawn(move || {
-        rt.block_on(async {
-            loop {
-                tokio::time::sleep(Duration::from_secs(3600)).await;
-            }
-        })
-    });
-
-    // Log to stdout (if you run with `RUST_LOG=debug`).
-    tracing_subscriber::fmt::init();
-
-    let native_options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "omni vanitas",
-        native_options,
-        Box::new(|_| Box::new(omnis_vanitas::TemplateApp::new())),
-    );
-}
-
-// when compiling to web using trunk.
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    let rt = Runtime::new().expect("Unable to create Runtime");
-
-    // Enter the runtime so that `tokio::spawn` is available immediately.
-    let _enter = rt.enter();
-
-    // Execute the runtime in its own thread.
-    // The future doesn't have to do anything. In this example, it just sleeps forever.
-    std::thread::spawn(move || {
-        rt.block_on(async {
-            loop {
-                tokio::time::sleep(Duration::from_secs(3600)).await;
-            }
-        })
-    });
-    // Make sure panics are logged using `console.error`.
 
     use eframe::{Theme, WebGlContextOption};
     use futures::executor;
