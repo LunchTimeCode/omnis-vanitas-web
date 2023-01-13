f5955b0a7b2534892d6cb7f8784292e30677adf8
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2023-01-13 15:06:48 +0100

    actually implement random walk

--- src/app.rs
+++ src/app.rs
@@ -3,10 +3,7 @@ use egui::{
     vec2, Ui,
 };
 
-use crate::{
-    random_cordinates::{random_cordinates_one_dim},
-    random_cordinates_two_dim,
-};
+use crate::{random_cordinates::random_cordinates_one_dim, random_cordinates_two_dim};
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 ///
@@ -18,12 +15,7 @@ pub struct WebApp {
 
     selected_random_walks: RandomWalkApps,
     walk_plot_state: Vec<[f64; 2]>,
-    from: f64,
-    to: f64,
-    steps: u64
-
-
-
+    steps: u64,
 }
 
 impl Default for WebApp {
@@ -33,8 +25,6 @@ impl Default for WebApp {
             selected: Apps::Welcome,
             selected_random_walks: RandomWalkApps::OneDimension,
             walk_plot_state: vec![[0.0, 0.0]],
-            from:0.0,
-            to: 0.0,
             steps: 0,
         }
     }
@@ -74,8 +64,6 @@ impl eframe::App for WebApp {
             selected,
             selected_random_walks: _,
             walk_plot_state,
-            from,
-            to,
             steps,
         } = self;
 
@@ -105,9 +93,12 @@ impl eframe::App for WebApp {
                 Apps::Welcome => {
                     ui.heading("Welcome to my webapp");
                 }
-                Apps::RandomWalks => {
-                    render_random_walk(ui, &mut self.selected_random_walks, walk_plot_state, from, to, steps)
-                }
+                Apps::RandomWalks => render_random_walk(
+                    ui,
+                    &mut self.selected_random_walks,
+                    walk_plot_state,
+                    steps,
+                ),
                 Apps::Second => place_holder(ui),
                 Apps::Third => place_holder(ui),
             }
@@ -138,7 +129,7 @@ fn render_random_walk(
     ui: &mut Ui,
     selected_random_walks: &mut RandomWalkApps,
     walk_plot_state: &mut Vec<[f64; 2]>,
-    from: &mut f64, to: &mut f64, steps: &mut u64,
+    steps: &mut u64,
 ) {
     egui::Grid::new("random_walk grid").show(ui, |ui| {
         ui.horizontal(|ui| {
@@ -163,46 +154,56 @@ fn render_random_walk(
         ui.end_row();
 
         ui.vertical(|ui| match selected_random_walks {
-            RandomWalkApps::OneDimension => render_random_walk_one_dimension(ui, walk_plot_state, from, to, steps),
-            RandomWalkApps::TwoDimensons => render_random_walk_two_dimension(ui, walk_plot_state,from, to, steps),
+            RandomWalkApps::OneDimension => {
+                render_random_walk_one_dimension(ui, walk_plot_state, steps)
+            }
+            RandomWalkApps::TwoDimensons => {
+                render_random_walk_two_dimension(ui, walk_plot_state,  steps)
+            }
         });
 
         ui.end_row();
     });
 }
 
-fn render_random_walk_two_dimension(ui: &mut Ui, walk_plot_state: &mut Vec<[f64; 2]>,from: &mut f64, to: &mut f64, steps: &mut u64,) {
-
-    let cordinates = random_cordinates_two_dim(*from, *to, *steps);
-
-    ui.vertical(|ui| {
-        ui.add_space(20.0);
-        plot_actions(ui, walk_plot_state,cordinates);
-        ui.end_row();
-        plot_settings(ui, from,  to, steps);
-        ui.end_row();
-        ui.horizontal(|ui| {
-            let sin: PlotPoints = PlotPoints::from(walk_plot_state.clone());
-
-            let line = Line::new(sin);
-            Plot::new("my_plot")
-                .view_aspect(2.0)
-                .min_size(vec2(1000.0, 250.0))
-                .show(ui, |plot_ui| plot_ui.line(line));
-        });
-        ui.end_row();
-    });
-}
-
-fn render_random_walk_one_dimension(ui: &mut Ui, walk_plot_state: &mut Vec<[f64; 2]>,from: &mut f64, to: &mut f64, steps: &mut u64,) {
-
-    let cordinates = random_cordinates_one_dim(*from, *to, *steps);
+fn render_random_walk_two_dimension(
+    ui: &mut Ui,
+    walk_plot_state: &mut Vec<[f64; 2]>,
+    steps: &mut u64,
+) {
+    let cordinates = random_cordinates_two_dim(*steps);
 
     ui.vertical(|ui| {
         ui.add_space(20.0);
         plot_actions(ui, walk_plot_state, cordinates);
         ui.end_row();
-        plot_settings(ui, from,  to, steps);
+        plot_settings(ui, steps);
+        ui.end_row();
+        ui.horizontal(|ui| {
+            let sin: PlotPoints = PlotPoints::from(walk_plot_state.clone());
+
+            let line = Line::new(sin);
+            Plot::new("my_plot")
+                .view_aspect(2.0)
+                .min_size(vec2(1000.0, 250.0))
+                .show(ui, |plot_ui| plot_ui.line(line));
+        });
+        ui.end_row();
+    });
+}
+
+fn render_random_walk_one_dimension(
+    ui: &mut Ui,
+    walk_plot_state: &mut Vec<[f64; 2]>,
+    steps: &mut u64,
+) {
+    let cordinates = random_cordinates_one_dim(*steps);
+
+    ui.vertical(|ui| {
+        ui.add_space(20.0);
+        plot_actions(ui, walk_plot_state, cordinates);
+        ui.end_row();
+        plot_settings(ui, steps);
         ui.end_row();
 
         ui.horizontal(|ui| {
@@ -218,16 +219,12 @@ fn render_random_walk_one_dimension(ui: &mut Ui, walk_plot_state: &mut Vec<[f64;
     });
 }
 
-fn plot_actions(
-    ui: &mut Ui,
-    walk_plot_state: &mut Vec<[f64; 2]>,
-    cordinates: Vec<[f64; 2]>
-) {
+fn plot_actions(ui: &mut Ui, walk_plot_state: &mut Vec<[f64; 2]>, cordinates: Vec<[f64; 2]>) {
     ui.horizontal(|ui| {
         ui.collapsing("Actions", |ui| {
             ui.horizontal_wrapped(|ui| {
                 ui.horizontal(|ui| {
-                    if ui.button("random plot").clicked() {
+                    if ui.button("walk").clicked() {
                         *walk_plot_state = cordinates;
                     };
                 });
@@ -236,18 +233,11 @@ fn plot_actions(
     });
 }
 
-fn plot_settings(
-    ui: &mut Ui,
-    from: &mut f64, to: &mut f64, steps: &mut u64,
-) {
+fn plot_settings(ui: &mut Ui, steps: &mut u64) {
     ui.horizontal(|ui| {
         ui.collapsing("Settings", |ui| {
             ui.horizontal_wrapped(|ui| {
                 ui.horizontal(|ui| {
-
-
-                    ui.add(egui::Slider::new(from, -10000.0..=*to -1.0).text("from"));
-                    ui.add(egui::Slider::new(to, -*from -1.0..=-10000.0).text("to"));
                     ui.add(egui::DragValue::new(steps));
                     ui.label("steps")
                 });
--- src/random_cordinates.rs
+++ src/random_cordinates.rs
@@ -1,25 +1,56 @@
 use rand::Rng;
 
-pub fn random_cordinates_two_dim(from: f64, to: f64, steps: u64) -> Vec<[f64; 2]> {
+pub fn random_cordinates_two_dim(steps: u64) -> Vec<[f64; 2]> {
     let mut ran = rand::thread_rng();
 
-    let result: Vec<[f64; 2]> = (0..steps)
-        .map(|_| {
-            let random_point: [f64; 2] = [ran.gen_range(from..to),ran.gen_range(from..to)];
-            random_point
-        })
-        .collect();
-    result
+    let x_walk: Vec<u64> = (0..steps).map(|_| ran.gen_range(0..=1)).collect();
+    let y_walk: Vec<u64> = (0..steps).map(|_| ran.gen_range(0..=1)).collect();
+
+    let walk: &mut Vec<[f64; 2]> = &mut vec![[0.0, 0.0]];
+
+    for i in 0..steps as usize {
+        let x: u64 = x_walk[i];
+        let y: u64 = y_walk[i];
+
+        let step_before_x= walk[i][0];
+        let step_before_y= walk[i][1];
+
+        let new_step_x= step_before_x + bool_as_number(x);
+        let new_step_y= step_before_y + bool_as_number(y);
+
+        let new_step = [new_step_x, new_step_y];
+        walk.push(new_step)
+    }
+
+    walk.clone()
 }
 
-pub fn random_cordinates_one_dim(from: f64, to: f64, steps: u64) -> Vec<[f64; 2]> {
+fn bool_as_number(value: u64) -> f64 {
+    if value == 1 {
+        1.0
+    } else {
+        -1.0
+    }
+}
+
+pub fn random_cordinates_one_dim(steps: u64) -> Vec<[f64; 2]> {
     let mut ran = rand::thread_rng();
 
-    let result: Vec<[f64; 2]> = (0..steps)
-        .map(|_| {
-            let random_point: [f64; 2] = [ran.gen_range(from..to), 0.0];
-            random_point
-        })
-        .collect();
-    result
+    let x_walk: Vec<u64> = (0..steps).map(|_| ran.gen_range(0..=1)).collect();
+    
+    let walk: &mut Vec<[f64; 2]> = &mut vec![[0.0, 0.0]];
+
+    for i in 0..steps as usize {
+        let x: u64 = x_walk[i];
+
+        let step_before_x= walk[i][0];
+
+        let new_step_x= step_before_x + bool_as_number(x);
+
+        let new_step = [new_step_x, 0.0];
+        walk.push(new_step)
+    }
+
+    walk.clone()
 }
+
