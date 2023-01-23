091d6105346cb777f7231143aebdcff2b4c764cd
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2023-01-23 13:59:30 +0100

    refactor: prepare for another app

--- src/app.rs
+++ src/app.rs
@@ -1,30 +1,19 @@
-use egui::{
-    plot::{Line, Plot, PlotPoints},
-    vec2, Ui,
-};
+use egui::Ui;
 
-use crate::{random_cordinates::random_cordinates_one_dim, random_cordinates_two_dim};
+use crate::{walk_app::render_random_walk, WalkApp};
 
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)]
 pub struct WebApp {
     selected: Apps,
-
-    selected_random_walks: RandomWalkApps,
-
-    #[serde(skip)]
-    walk_plot_state: Vec<[f64; 2]>,
-
-    steps: u64,
+    walk_app: WalkApp,
 }
 
 impl Default for WebApp {
     fn default() -> Self {
         Self {
             selected: Apps::Welcome,
-            selected_random_walks: RandomWalkApps::OneDimension,
-            walk_plot_state: vec![[0.0, 0.0]],
-            steps: 0,
+            walk_app: WalkApp::default(),
         }
     }
 }
@@ -43,8 +32,6 @@ impl WebApp {
 enum Apps {
     Welcome,
     RandomWalks,
-    Second,
-    Third,
 }
 
 impl eframe::App for WebApp {
@@ -53,192 +40,51 @@ impl eframe::App for WebApp {
     }
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self {
-            selected,
-            selected_random_walks: _,
-            walk_plot_state,
-            steps,
-        } = self;
-
         egui::TopBottomPanel::top("top_grid").show(ctx, |ui| {
             ui.add_space(3.0);
-            egui::Grid::new("apps_grid").show(ui, |ui| {
-                ui.horizontal(|ui| {
-                    ui.heading("Applications");
-                    if ui.button("Welcome").clicked() {
-                        *selected = Apps::Welcome;
-                    };
-                    if ui.button("Random Walks").clicked() {
-                        *selected = Apps::RandomWalks;
-                    };
-                });
-
-                ui.end_row();
-            });
-
+            render_selection(self, ui);
             ui.add_space(3.0);
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.add_space(20.0);
-
-            match selected {
-                Apps::Welcome => {
-                    ui.heading("Welcome to my webapp");
-                }
-                Apps::RandomWalks => {
-                    render_random_walk(ui, &mut self.selected_random_walks, walk_plot_state, steps)
-                }
-                Apps::Second => place_holder(ui),
-                Apps::Third => place_holder(ui),
-            }
-
-            ui.horizontal(|ui| {
-                ui.spacing_mut().item_spacing.x = 0.0;
-                ui.label("powered by ");
-                ui.hyperlink_to("Silen", "https://github.com/SilenLoc/omnis-vanitas-web");
-                ui.label(".");
-            });
-
+            render_selected(self, ui);
+            render_powered_by(ui);
             egui::warn_if_debug_build(ui);
         });
     }
 }
 
-fn place_holder(ui: &mut Ui) {
-    ui.heading("Place Holder");
-}
-
-#[derive(Debug, PartialEq, serde::Deserialize, serde::Serialize)]
-enum RandomWalkApps {
-    OneDimension,
-    TwoDimensons,
-}
-
-fn render_random_walk(
-    ui: &mut Ui,
-    selected_random_walks: &mut RandomWalkApps,
-    walk_plot_state: &mut Vec<[f64; 2]>,
-    steps: &mut u64,
-) {
-    egui::Grid::new("random_walk grid").show(ui, |ui| {
+fn render_selection(web_app: &mut WebApp, ui: &mut Ui) {
+    egui::Grid::new("apps_grid").show(ui, |ui| {
         ui.horizontal(|ui| {
-            ui.heading("Random Walks");
-
-            egui::ComboBox::from_label("")
-                .selected_text(format!("{:?}", selected_random_walks))
-                .show_ui(ui, |ui| {
-                    ui.selectable_value(
-                        selected_random_walks,
-                        RandomWalkApps::OneDimension,
-                        "One dimensions",
-                    );
-                    ui.selectable_value(
-                        selected_random_walks,
-                        RandomWalkApps::TwoDimensons,
-                        "Two dimensions",
-                    );
-                });
-        });
-
-        ui.end_row();
-
-        ui.vertical(|ui| match selected_random_walks {
-            RandomWalkApps::OneDimension => {
-                render_random_walk_one_dimension(ui, walk_plot_state, steps)
-            }
-            RandomWalkApps::TwoDimensons => {
-                render_random_walk_two_dimension(ui, walk_plot_state, steps)
-            }
+            ui.heading("Applications");
+            if ui.button("Welcome").clicked() {
+                web_app.selected = Apps::Welcome;
+            };
+            if ui.button("Random Walks").clicked() {
+                web_app.selected = Apps::RandomWalks;
+            };
         });
 
         ui.end_row();
     });
 }
 
-fn render_random_walk_one_dimension(
-    ui: &mut Ui,
-    walk_plot_state: &mut Vec<[f64; 2]>,
-    steps: &mut u64,
-) {
-    ui.vertical(|ui| {
-        ui.add_space(20.0);
-        plot_actions(ui, walk_plot_state, steps, 1);
-        ui.end_row();
-        plot_settings(ui, steps);
-        ui.end_row();
-
-        ui.horizontal(|ui| {
-            let sin: PlotPoints = PlotPoints::from(walk_plot_state.clone());
-
-            let line = Line::new(sin);
-            Plot::new("my_plot")
-                .view_aspect(2.0)
-                .min_size(vec2(1000.0, 250.0))
-                .show(ui, |plot_ui| plot_ui.line(line));
-        });
-        ui.end_row();
-    });
+fn render_selected(web_app: &mut WebApp, ui: &mut Ui) {
+    match web_app.selected {
+        Apps::Welcome => {
+            ui.heading("Welcome to my webapp");
+        }
+        Apps::RandomWalks => render_random_walk(&mut web_app.walk_app, ui),
+    }
 }
 
-fn render_random_walk_two_dimension(
-    ui: &mut Ui,
-    walk_plot_state: &mut Vec<[f64; 2]>,
-    steps: &mut u64,
-) {
-    ui.vertical(|ui| {
-        ui.add_space(20.0);
-        plot_actions(ui, walk_plot_state, steps, 2);
-        ui.end_row();
-        plot_settings(ui, steps);
-        ui.end_row();
-        ui.horizontal(|ui| {
-            let sin: PlotPoints = PlotPoints::from(walk_plot_state.clone());
-
-            let line = Line::new(sin);
-            Plot::new("my_plot")
-                .view_aspect(2.0)
-                .min_size(vec2(1000.0, 250.0))
-                .show(ui, |plot_ui| plot_ui.line(line));
-        });
-        ui.end_row();
-    });
-}
-
-fn plot_actions(
-    ui: &mut Ui,
-    walk_plot_state: &mut Vec<[f64; 2]>,
-    steps: &mut u64,
-    dimensions: u128,
-) {
+fn render_powered_by(ui: &mut Ui) {
     ui.horizontal(|ui| {
-        ui.collapsing("Actions", |ui| {
-            ui.horizontal_wrapped(|ui| {
-                ui.horizontal(|ui| {
-                    if ui.button("walk").clicked() {
-                        let cords = match dimensions {
-                            1 => random_cordinates_one_dim(*steps),
-                            2 => random_cordinates_two_dim(*steps),
-                            _ => vec![],
-                        };
-
-                        *walk_plot_state = cords;
-                    };
-                });
-            });
-        });
-    });
-}
-
-fn plot_settings(ui: &mut Ui, steps: &mut u64) {
-    ui.horizontal(|ui| {
-        ui.collapsing("Settings", |ui| {
-            ui.horizontal_wrapped(|ui| {
-                ui.horizontal(|ui| {
-                    ui.add(egui::DragValue::new(steps));
-                    ui.label("steps")
-                });
-            });
-        });
+        ui.spacing_mut().item_spacing.x = 0.0;
+        ui.label("powered by ");
+        ui.hyperlink_to("Silen", "https://github.com/SilenLoc/omnis-vanitas-web");
+        ui.label(".");
     });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 
 mod app;
 pub use app::WebApp;
+mod walk_app;
+pub use walk_app::WalkApp;
 mod random_cordinates;
 pub use random_cordinates::random_cordinates_one_dim;
 pub use random_cordinates::random_cordinates_two_dim;
