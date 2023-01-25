d13092b4c84dee0be5d1a264bea37cb1947e7493
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2023-01-25 11:19:50 +0100

    feat: add tagsymbols

--- src/git_app.rs
+++ src/git_app.rs
@@ -1,15 +1,19 @@
+use eframe::epaint::ahash::HashMap;
 use egui::Ui;
 
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)]
 pub struct GitApp {
     selected_gits_app: GitApps,
+    #[serde(skip)]
+    choosen: ChoosenTagSymbols,
 }
 
 impl Default for GitApp {
     fn default() -> Self {
         Self {
             selected_gits_app: GitApps::TagDiffWeb,
+            choosen: ChoosenTagSymbols::default(),
         }
     }
 }
@@ -44,18 +48,29 @@ pub fn render_git_app(app: &mut GitApp, ui: &mut Ui) {
         ui.end_row();
 
         ui.vertical(|ui| match app.selected_gits_app {
-            GitApps::TagDiffWeb => render_web_diff(ui),
-            GitApps::TagDiffCommand => render_tag_diff(ui),
+            GitApps::TagDiffWeb => render_web_diff(app, ui),
+            GitApps::TagDiffCommand => render_tag_diff(app, ui),
         });
 
         ui.end_row();
     });
 }
 
-fn render_tag_diff(ui: &mut Ui) {
+fn render_tag_diff(app: &mut GitApp, ui: &mut Ui) {
     ui.vertical(|ui| {
         ui.add_space(20.0);
-        actions(ui);
+        actions(app, ui);
+        ui.end_row();
+        settings(ui);
+        ui.label(&app.choosen.get_choosen_symbol_chain());
+        ui.end_row();
+    });
+}
+
+fn render_web_diff(app: &mut GitApp, ui: &mut Ui) {
+    ui.vertical(|ui| {
+        ui.add_space(20.0);
+        actions(app, ui);
         ui.end_row();
         settings(ui);
         ui.end_row();
@@ -64,24 +79,17 @@ fn render_tag_diff(ui: &mut Ui) {
     });
 }
 
-fn render_web_diff(ui: &mut Ui) {
-    ui.vertical(|ui| {
-        ui.add_space(20.0);
-        actions(ui);
-        ui.end_row();
-        settings(ui);
-        ui.end_row();
-
-        ui.end_row();
-    });
-}
-
-fn actions(ui: &mut Ui) {
+fn actions(app: &mut GitApp, ui: &mut Ui) {
     ui.horizontal(|ui| {
         ui.collapsing("Actions", |ui| {
             ui.horizontal_wrapped(|ui| {
                 ui.horizontal(|ui| {
-                    if ui.button("create").clicked() {};
+                    if ui.button("create").clicked() {
+                        app.choosen.choose(TagSymbol::new(1, "prefix", "something"));
+                        app.choosen.choose(TagSymbol::new(2, "delitmiter", "/"));
+                        app.choosen
+                            .choose(TagSymbol::new(3, "version", "someversion"));
+                    };
                 });
             });
         });
@@ -98,4 +106,56 @@ fn settings(ui: &mut Ui) {
             });
         });
     });
+}
+
+#[derive(Clone)]
+pub struct TagSymbol {
+    order: u16,
+    name: String,
+    symbol: String,
+}
+
+impl TagSymbol {
+    fn new(order: u16, name: &str, symbol: &str) -> TagSymbol {
+        TagSymbol {
+            order,
+            name: name.to_owned(),
+            symbol: symbol.to_owned(),
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct ChoosenTagSymbols {
+    pub symbols: HashMap<String, TagSymbol>,
+}
+
+impl ChoosenTagSymbols {
+    fn choose(&mut self, choose: TagSymbol) {
+        self.symbols.insert(choose.name.clone(), choose);
+    }
+
+    fn get_choosen_symbol_chain(&self) -> String {
+        let mut values: Vec<TagSymbol> = self.symbols.values().cloned().collect();
+        values.sort_by(|a, b| a.order.cmp(&b.order));
+        let strings: Vec<String> = values.into_iter().map(|tuple| tuple.symbol).collect();
+        strings.concat()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn should_return_symbols_in_correct_order() {
+        let mut chooser = ChoosenTagSymbols::default();
+        chooser.choose(TagSymbol::new(1, "prefix", "something"));
+        chooser.choose(TagSymbol::new(2, "delitmiter", "/"));
+        chooser.choose(TagSymbol::new(3, "version", "someversion"));
+
+        let tag = chooser.get_choosen_symbol_chain();
+        assert_eq!(tag, "something/version/")
+    }
 }
